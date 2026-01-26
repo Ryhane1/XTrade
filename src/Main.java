8a3eb49c26364996eb6ce82694a2f0673a4f9eaa
Ryhane <ryhane.harchi@gmail.com>
2026-01-26 18:00:02 +0100

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,57 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner add = new Scanner(System.in);
+        int choix ;
+        System.out.println("");
+        System.out.println("2. Admine ");
+        System.out.println("1. Trader ");
+        choix = add.nextInt();
 
     }
+    public static void menuPrincipal(){
+        Scanner add = new Scanner(System.in);
+        int choix ;
+        System.out.println("");
+        System.out.println("2. Admine ");
+        System.out.println("1. Trader ");
+        choix = add.nextInt();
+        if (choix == 1){
+
+        } else if (choix == 2) {
+
+        }else {
+            System.out.println("Choix Invalid !!");
+        }
+    }
+    public static int menuAdmin(){
+        Scanner add = new Scanner(System.in);
+        int choix ;
+        System.out.println("");
+        System.out.println("2. Ajouter Person ");
+        System.out.println("1. Ajouter Trader ");
+        System.out.println("1. Ajouter Portfolio ");
+        System.out.println("1. Ajouter Action ");
+        System.out.println("1. Ajouter Crypto ");
+        System.out.println("1. Supprimer Portfolio ");
+        System.out.println("1. Supprimer Trader ");
+        return choix = add.nextInt();
+
+    }
+    public static int menuTrader(){
+        Scanner add = new Scanner(System.in);
+        int choix ;
+        System.out.println("");
+        System.out.println("2. Acheter Action ");
+        System.out.println("1. Acheter Crypto ");
+        System.out.println("1. Consulter Portfolio ");
+        System.out.println("1. Consulter Historique Transaction ");
+        System.out.println("1.  ");
+        System.out.println("1. Supprimer Portfolio ");
+        System.out.println("1. Supprimer Trader ");
+        return choix = add.nextInt();
+    }
+
 }
--- src/Trader.java
+++ src/Trader.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Trader extends Person {
     private int id ;
-
+    static ArrayList<Person> traders = new ArrayList<>();
+    Scanner add = new Scanner(System.in);
     public Trader(String nom, String mail, int id) {
         super(nom, mail);
         this.id = id;
@@ -13,4 +18,15 @@ public class Trader extends Person {
     public void setId(int id) {
         this.id = id;
     }
+    public static void addTrader(Scanner add){
+        System.out.println("Entre le nom : ");
+        String nom = add.nextLine();
+        System.out.println("Entre Mail : ");
+        String mail = add.nextLine();
+        int id = new Random().nextInt(100000,1000000);
+        System.out.println("Id Générer !!");
+        traders.add(new Trader(nom,mail,id));
+
+    }
+
 }
